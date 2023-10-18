97df75dc5cc3ff7148e6679480827a6ca5dd2f9f
cleidson-cmd <cleidson.santana@unidompedro.com>
2023-10-17 21:27:05 -0300

    base para api 3

---
+++ vacinacao/src/main/java/br/com/gerenciarvacinacao/vacinacao/VacinacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerenciarvacinacao.vacinacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacinacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacinacaoApplication.class, args);
+	}
+
+}
